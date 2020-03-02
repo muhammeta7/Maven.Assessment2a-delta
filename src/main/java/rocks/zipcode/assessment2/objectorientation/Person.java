28779da03be8600fd69b1abc80ce5f87f200ec53
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-02 11:52:33 -0500

    person and animal class complete

--- src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
+++ src/main/java/rocks/zipcode/assessment2/objectorientation/Person.java
@@ -23,33 +23,92 @@ public class Person {
 
     public Person() {
         this.id = Long.MIN_VALUE;
-        this.name = " ";
+        this.name = "";
         this.address = new Address();
     }
 
     public Long getId() {
-        return null;
+        return this.id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Address getAddress() {
-        return null;
+        return this.address;
     }
 
     public void setAddress(Address address) {
+        this.address = address;
     }
 
     @Override
     public boolean equals(Object o) {
-        return (Boolean)null;
+        if (id == null) {
+            if (((Person) o).id != null) {
+                return false;
+            }
+        }
+        if (((Person) o).id == null) {
+            if (id != null) {
+                return false;
+            }
+        } else {
+            if (!(id.equals(((Person) o).id))) {
+                return false;
+            }
+        }
+
+        if (name == null) {
+            if (((Person) o).name != null) {
+                return false;
+            }
+        }
+        if (((Person) o).name == null) {
+            if (name != null) {
+                return false;
+            }
+        } else {
+            if (!this.name.equals(((Person) o).name)) {
+                return false;
+            }
+        }
+
+        if (address == null) {
+            if (((Person) o).address != null) {
+                return false;
+            }
+        }
+
+        if (((Person) o).address == null) {
+            return address == null;
+        } else {
+            if (!(address.equals(((Person) o).address))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString(){
+
+        String personInfo = "Person{";
+        String idString = "id=" + id + ", ";
+        String nameString = "name='" + name + "', ";
+        String addressString = "address=" + address.toString() + "}";
+        personInfo += idString + nameString + addressString;
+        return personInfo;
+
     }
 }
